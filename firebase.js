e9121cb9d160eecf278533b83a11d6d7a81eb601
kilik42 <maevins@cps.edu>
2023-02-03 10:41:31 -0600

    first commit

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD8r5PXADyX3eqsmUniYV05PqKmq9Av6mY",
+    authDomain: "kid-tok-d6c8d.firebaseapp.com",
+    projectId: "kid-tok-d6c8d",
+    storageBucket: "kid-tok-d6c8d.appspot.com",
+    messagingSenderId: "77768471598",
+    appId: "1:77768471598:web:02711ffe28b4681e7c5bbf",
+    measurementId: "G-VR0ZFLXE6L"
+  };
---
+++ src/Video.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Video.css'
+function Video() {
+  return (
+    <div className='video'>
+            <h1>this is a video</h1>
+
+        {/* video footer */}
+        
+        
+        {/* video sidebar */}
+
+    </div>
+  )
+}
+
+export default Video
