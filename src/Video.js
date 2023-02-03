2eebc9d70d625275b83f8437d01a7a2ab48158c6
kilik42 <maevins@cps.edu>
2023-02-03 11:25:40 -0600

    working on the video

--- src/Video.js
+++ src/Video.js
@@ -4,10 +4,12 @@ function Video() {
   return (
     <div className='video'>
             <h1>this is a video</h1>
+        <video className = "video__player"src="https://flip.com/s/W2tM57jSNWom">
 
+        </video>
         {/* video footer */}
         
-        
+
         {/* video sidebar */}
 
     </div>
